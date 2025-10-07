be325072a29e14819095c05c41b5fd130932a2f4
JohnTheWeak <143750773+JohnTheWeak@users.noreply.github.com>
2025-10-07 19:51:46 +0800

    Add files via upload

---
+++ Move_Files.ps1
@@ -0,0 +1,7 @@
+﻿# This ps.1 move multiple file from one location to another
+# You will need first create a txt.file that list all the file paths of the targeted files
+
+$Mergefile = Get-Content -Path "C:\Users\Admin\Desktop\Path.txt" #Create an variable to store the .txt
+foreach ($file in $Mergefile) {
+    Move-Item -Path $file -Destination "C:\Users\Admin\Desktop\IT_Skills"
+}
