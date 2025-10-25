d163255106424a7aa614b16692eb9293c555f57d
JohnTheWeak <143750773+JohnTheWeak@users.noreply.github.com>
2025-10-25 15:31:10 +0800

    Add files via upload

---
+++ CombineExcelWorksheet.ps1
@@ -0,0 +1,6 @@
+﻿
+    $data1 = Import-Excel -Path "C:\Users\Admin\Desktop\Practices\W1.xlsx" -WorksheetName 'Sheet1'
+    $data2 = Import-Excel -Path "C:\Users\Admin\Desktop\Practices\W2.xlsx" -WorksheetName 'Sheet2'
+    
+    $data1 | Export-Excel -Path "C:\Users\Admin\Desktop\Practices\Combined_data.xlsx" -WorksheetName 'Com1'
+    $data2 | Export-Excel -Path "C:\Users\Admin\Desktop\Practices\Combined_data.xlsx" -WorksheetName 'Com2'
