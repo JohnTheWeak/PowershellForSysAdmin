c822112d2fd9d8a9ad8445545fd614e3db3f3b4d
JohnTheWeak <143750773+JohnTheWeak@users.noreply.github.com>
2025-10-25 15:32:54 +0800

    Update Move_Files.ps1

--- Move_Files.ps1
+++ Move_Files.ps1
@@ -1,4 +1,4 @@
-﻿# This ps.1 move multiple files from one location to another
+﻿# This function moves multiple files from one location to another
 # You will need first create a txt.file that list all the file paths of the targeted files
 
 $Mergefile = Get-Content -Path "C:\Users\Admin\Desktop\Path.txt" #Create an variable to store the .txt
@@ -6,3 +6,4 @@ foreach ($file in $Mergefile) {
     Move-Item -Path $file -Destination "C:\Users\Admin\Desktop\IT_Skills"
 
 }
+
